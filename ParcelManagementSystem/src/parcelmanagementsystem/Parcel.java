26d9eb3b88f3397581efbc191789db3db61826e5
Manjanpadu <s251594704@studentmail.unimap.edu.my>
2026-09-10 20:48:36 +0800

    Update Parcel.java

--- ParcelManagementSystem/src/parcelmanagementsystem/Parcel.java
+++ ParcelManagementSystem/src/parcelmanagementsystem/Parcel.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package parcelmanagementsystem;
 
-/**
- *
- * @author USER
- */
 public class Parcel {
-    
+
+    private int trackingNo;
+    private String senderName;
+    private String receiverName;
+    private double weight;
+    private String status;
+
+    public Parcel(int trackingNo, String senderName,
+                  String receiverName, double weight,
+                  String status) {
+
+        this.trackingNo = trackingNo;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.weight = weight;
+        this.status = status;
+    }
+
+    public int getTrackingNo() {
+        return trackingNo;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setTrackingNo(int trackingNo) {
+        this.trackingNo = trackingNo;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double calculateDeliveryFee() {
+        return 0.0;
+    }
+
+    public String getParcelType() {
+        return "Parcel";
+    }
 }
