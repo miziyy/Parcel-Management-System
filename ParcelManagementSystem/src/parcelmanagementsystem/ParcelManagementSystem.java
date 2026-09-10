940b564a305eb07b565cdbf2be69a9045685d855
Manjanpadu <s251594704@studentmail.unimap.edu.my>
2026-09-10 20:49:44 +0800

    Update ParcelManagementSystem.java

--- ParcelManagementSystem/src/parcelmanagementsystem/ParcelManagementSystem.java
+++ ParcelManagementSystem/src/parcelmanagementsystem/ParcelManagementSystem.java
@@ -1,12 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * MUHAMMAD AIMAN BIN AZIZAN-251594704
  */
 package parcelmanagementsystem;
 
 /**
  *
- * @author USER
+ * 
  */
 public class ParcelManagementSystem {
 
